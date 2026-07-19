485d8539708e3647eecfe33b4a950e3824935789
oceanicdayi <oceanicdayi@gmail.com>
2026-07-19 15:21:40 +0800

    Initialize seismology Python package

---
+++ src/seismo/__init__.py
@@ -0,0 +1,5 @@
+"""Reusable seismology helpers for the Learning OpenCode project."""
+
+from .completeness import estimate_mc_maxc, monthly_moving_mc
+
+__all__ = ["estimate_mc_maxc", "monthly_moving_mc"]
